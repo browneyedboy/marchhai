f69f96b61ef8237bc434a0526eeab9aa69e0695e
Amarsanaa <amar@grafxs-iMac.local>
2016-12-24 20:18:56 +0800

    lgns and notwork

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormGroup, FormControl } from '@angular/forms';
 
-/*
-  Generated class for the Login page.
+import { RegisterPage } from '../register/register';
+import { TabsPage } from '../tabs/tabs';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+	todo: FormGroup;
+  constructor(public navCtrl: NavController, public http: Http) {
+  	this.todo = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello LoginPage Page');
   }
 
+  ionViewDidLoad() {
+    
+  }
+
+  gotoregister(){
+  	this.navCtrl.push(RegisterPage);
+  }
+
+  dologin(){
+  	// index.php/api/login
+  	//{email: <USER EMAIL>, password: <USER PASSWORD>, token=<M@RCH@@KH@!@P!>}
+	console.log(this.todo.value.email);  	
+	console.log(this.todo.value.password);
+
+	this.navCtrl.push(TabsPage);
+
+	// var loginServiceData = {
+	//     email: this.email,
+	//     password: this.password,
+	//     token: 'M@RCH@@KH@!@P!'
+	// };
+
+	// 	this.http.post('http://www.marchaahai.mn/index.php/api/login', loginServiceData).map(
+		// res => res.json()).subscribe(data => {
+	//       console.log(data.response);
+	//   },
+	//   err => {
+	//       console.log("Oops!");
+	//   });
+  }
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormGroup, FormControl } from '@angular/forms';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 /*
   Generated class for the Register page.
 
@@ -12,11 +15,58 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'register.html'
 })
 export class RegisterPage {
+	registerform: FormGroup;
+	constructor(public navCtrl: NavController, public http: Http) {
+		this.registerform = new FormGroup({
+			username: new FormControl('', Validators.required),
+	  		useremail: new FormControl('', Validators.required),
+	  		userpassword: new FormControl('', Validators.required),
+	  		userphone: new FormControl('', Validators.required),
+	  		userage: new FormControl('', Validators.required),
+	  		usersex: new FormControl('', Validators.required)
+		});
+	}
 
-  constructor(public navCtrl: NavController) {}
+	ionViewDidLoad() {
+		console.log('Hello RegisterPage Page');
+	}
+	registerdo(){
+		//index.php/api/signup
+		// {username: <USERNAME>, 
+			// email: <USER EMAIL>, 
+			// phone_number: <USER PHONE>, 
+			// child_birthday: <USER CHILD BIRTHDAY>, 
+			// child_gender: <USER CHILD GENDER>, 
+			// password: <USER PASSWORD>, 
+			// token=<M@RCH@@KH@!@P!>}
 
-  ionViewDidLoad() {
-    console.log('Hello RegisterPage Page');
-  }
+		console.log(this.registerform.value.useremail);  	
+		console.log(this.registerform.value.userpassword);
+
+		var loginServiceData = JSON.stringify({
+			username: this.registerform.value.username,
+		    email: this.registerform.value.useremail,
+		    password: this.registerform.value.userpassword,
+		    phone_number: this.registerform.value.userphone,
+		    child_birthday: this.registerform.value.userage,
+		    child_gender: this.registerform.value.usersex,
+		    token: 'M@RCH@@KH@!@P!'
+		});
+		
+		this.http.post('http://www.marchaahai.mn/index.php/api/login', loginServiceData)
+        .subscribe(data => {
+        	console.log(data);
+        }, error => {
+            console.log("Oooops!");
+        });
+
+		// this.http.post('http://www.marchaahai.mn/index.php/api/login', loginServiceData).map(
+		// 	res => res.json()).subscribe(data => {
+	 //        	console.log(data.response);
+	 //    	},
+	 //    	err => {
+	 //        	console.log("Oops!");
+	 //    	});
+	}
 
 }
