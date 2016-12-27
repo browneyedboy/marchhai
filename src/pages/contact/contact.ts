9b93119a5e031f934ce3e5c6cc33f43f6a10ed8f
Amarsanaa <amar@grafxs-iMac.local>
2016-12-27 12:42:18 +0800

    okendok

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, Platform } from 'ionic-angular';
-
+import {global} from "../../app/global";
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
@@ -9,7 +9,8 @@ import { NavController, Platform } from 'ionic-angular';
 export class ContactPage {
 
   constructor(public navCtrl: NavController, public platform: Platform) {
-
+  	var userdata = global.userdetailget();
+  	console.log(userdata);
   }
   exitApp(){
     this.platform.exitApp();
